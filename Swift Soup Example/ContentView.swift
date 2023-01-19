ff46e3ce49895364e71988cc47c8ec6c3352ee31
Giorgio Giannotta <jorge_upa@msn.com>
2023-01-19 19:39:41 +0100

    Initial Commit

---
+++ Swift Soup Example/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Swift Soup Example
+//
+//  Created by Giorgio Giannotta on 19/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Soup Example/Swift_Soup_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Soup_ExampleApp.swift
+//  Swift Soup Example
+//
+//  Created by Giorgio Giannotta on 19/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Soup_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
